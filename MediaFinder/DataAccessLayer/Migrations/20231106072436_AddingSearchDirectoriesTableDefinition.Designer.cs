99ed8a20ae01c94db4c3d32ba531351e8b2cf7b0
Matthew McAtamney-Greenwood <matthew@thepinkmile.onmicrosoft.com>
2023-11-06 07:26:24 +0000

    Added missing db migrations for SearchDirectory table

--- MediaFinder/DataAccessLayer/Migrations/20231106072436_AddingSearchDirectoriesTableDefinition.Designer.cs
+++ MediaFinder/DataAccessLayer/Migrations/20231106072436_AddingSearchDirectoriesTableDefinition.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MediaFinder_v2.DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MediaFinder_v2.DataAccessLayer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231106072436_AddingSearchDirectoriesTableDefinition")]
+    partial class AddingSearchDirectoriesTableDefinition
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MediaFinder/DataAccessLayer/Migrations/20231106072436_AddingSearchDirectoriesTableDefinition.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaFinder_v2.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingSearchDirectoriesTableDefinition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SearchDirectory_SearchSettings_SettingsId",
+                table: "SearchDirectory");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SearchDirectory",
+                table: "SearchDirectory");
+
+            migrationBuilder.RenameTable(
+                name: "SearchDirectory",
+                newName: "SearchDirectories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SearchDirectory_SettingsId",
+                table: "SearchDirectories",
+                newName: "IX_SearchDirectories_SettingsId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SearchDirectories",
+                table: "SearchDirectories",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SearchDirectories_SearchSettings_SettingsId",
+                table: "SearchDirectories",
+                column: "SettingsId",
+                principalTable: "SearchSettings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SearchDirectories_SearchSettings_SettingsId",
+                table: "SearchDirectories");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SearchDirectories",
+                table: "SearchDirectories");
+
+            migrationBuilder.RenameTable(
+                name: "SearchDirectories",
+                newName: "SearchDirectory");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SearchDirectories_SettingsId",
+                table: "SearchDirectory",
+                newName: "IX_SearchDirectory_SettingsId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SearchDirectory",
+                table: "SearchDirectory",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SearchDirectory_SearchSettings_SettingsId",
+                table: "SearchDirectory",
+                column: "SettingsId",
+                principalTable: "SearchSettings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
