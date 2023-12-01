ea9d00b2b168cab9d43c300006709d7e680e4755
Izzy Coding <matthew@thepinkmile.org.uk>
2023-12-01 21:23:27 +0000

    Updated to use CsWin32 preview.

--- MediaFinder.Controls.Wpf/Converters/FormatKbSizeConverter.cs
+++ MediaFinder.Controls.Wpf/Converters/FormatKbSizeConverter.cs
@@ -1,8 +1,10 @@
-﻿using System.Globalization;
+using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Data;
 using System.Windows;
+
+using Windows.Win32;
 
 namespace MediaFinder.Controls.Wpf.Converters;
 
@@ -12,12 +14,17 @@ public class FormatKbSizeConverter : IValueConverter
     private static extern long StrFormatByteSizeW(long qdw, [MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszBuf,
         int cchBuf);
 
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Windows only application")]
+    public unsafe object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        var number = System.Convert.ToInt64(value ?? 0);
-        var sb = new StringBuilder(32);
-        StrFormatByteSizeW(number, sb, sb.Capacity);
-        return sb.ToString();
+        var buffer = new char[32];
+        var number = System.Convert.ToInt64(value);
+        fixed (char* pBuff = buffer)
+        {
+            _ = PInvoke.StrFormatByteSize(number, pBuff, 32);
+        }
+
+        return new string(buffer);
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
