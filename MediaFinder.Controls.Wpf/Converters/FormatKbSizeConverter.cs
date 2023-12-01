1375adbb385458591b33a681a2bbad30b008dea7
Izzy Coding <matthew@thepinkmile.org.uk>
2023-12-01 21:28:27 +0000

    Removing old implementation code.

--- MediaFinder.Controls.Wpf/Converters/FormatKbSizeConverter.cs
+++ MediaFinder.Controls.Wpf/Converters/FormatKbSizeConverter.cs
@@ -1,6 +1,4 @@
 ﻿using System.Globalization;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Windows.Data;
 using System.Windows;
 
@@ -10,10 +8,6 @@ namespace MediaFinder.Controls.Wpf.Converters;
 
 public class FormatKbSizeConverter : IValueConverter
 {
-    [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
-    private static extern long StrFormatByteSizeW(long qdw, [MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszBuf,
-        int cchBuf);
-
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Windows only application")]
     public unsafe object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
