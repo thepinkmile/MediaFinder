156cbb09de4b0a681852e01663704311ef8647b9
Izzy Coding <matthew@thepinkmile.org.uk>
2024-01-02 22:53:58 +0000

    Updating DiscoveryRuns definition with ConfigurationId and Working Directory.

--- MediaFinder.DataAccessLayer/Models/DiscoveryExecution.cs
+++ MediaFinder.DataAccessLayer/Models/DiscoveryExecution.cs
@@ -4,7 +4,10 @@ public class DiscoveryExecution
 {
     public Guid Id { get; set; }
 
+    public virtual int ConfigurationId { get; set; }
     public virtual SearchSettings Configuration { get; set; } = null!;
+
+    public required string WorkingDirectory { get; set; }
 
     public DateTimeOffset StartDateTime { get; set; } = DateTimeOffset.UtcNow;
 }
