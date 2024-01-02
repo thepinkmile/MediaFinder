2ee2ddba31c4854c9e1e1f00f3ac788a2dd1f209
Izzy Coding <matthew@thepinkmile.org.uk>
2024-01-02 21:33:03 +0000

    Adding initial discovery runs database table and models.

---
+++ MediaFinder.DataAccessLayer/Models/DiscoveryExecution.cs
@@ -0,0 +1,10 @@
+﻿namespace MediaFinder.DataAccessLayer.Models;
+
+public class DiscoveryExecution
+{
+    public Guid Id { get; set; }
+
+    public virtual SearchSettings Configuration { get; set; } = null!;
+
+    public DateTimeOffset StartDateTime { get; set; } = DateTimeOffset.UtcNow;
+}
