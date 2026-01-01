c22c4bcd1fa486704837a8e44c3d9d17fe1e0b93
The Pink Mile Developer <thepinkmile@users.noreply.github.com>
2026-01-01 18:10:19 +0000

    Updated to use CsWin32 preview. (#6)
    * Updated to use CsWin32 preview.
    
    * Removing old implementation code.
    
    * Minor update to remove warning suppression.
    
    * Adding wrapper for CsWin32 implementations.

---
+++ MediaFinder.Controls.Wpf/NativeMethodsExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Buffers;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Windows.Win32;
+
+internal static partial class PInvoke
+{
+    internal static string StrFormatByteSize<T>(T number, int bufferSize = 32)
+        where T : INumber<T>
+        => StrFormatByteSize(Convert.ToInt64(number), bufferSize);
+
+    internal unsafe static string StrFormatByteSize(long number, int bufferSize = 32)
+    {
+        var buffer = ArrayPool<char>.Shared.Rent(bufferSize);
+        try
+        {
+            fixed (char* ptr = buffer)
+            {
+                Debug.Assert(OperatingSystem.IsWindowsVersionAtLeast(5));
+                var pBuf = StrFormatByteSize(number, ptr, (uint)bufferSize);
+
+                return pBuf.ToString();
+            }
+        }
+        finally
+        {
+            ArrayPool<char>.Shared.Return(buffer);
+        }
+    }
+}
